7a24c2120160ef8e85a22ccb83885003e137297b
jhj5445 <56852841+jhj5445@users.noreply.github.com>
2026-01-19 17:29:01 +0900

    ddd

---
+++ qlib/contrib/model/pytorch_transformer.py
@@ -0,0 +1,8 @@
+
+from qlib.model.base import Model
+
+class TransformerModel(Model):
+    def __init__(self, *args, **kwargs): pass
+
+class Transformer(Model):
+    def __init__(self, *args, **kwargs): pass
